26be9199b9aa03cb2f8f8912530e02edb62f7d31
wt <oksvip@sina.com>
2017-05-05 18:16:53 +0800

    database link information

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * TouchPhp配置信息
+ * User: WT
+ * Date: 2017/5/5
+ * Time: 16:09
+ */
+
+return array(
+	//数据库信息
+	'dbHost'	=>	'127.0.0.1',
+	'dbUser'	=>	'root',
+	'dbPwd'		=>	'',
+	'dbName'	=>	'zhcdb'
+);
